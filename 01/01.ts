3d6b992746246b3e95a000462b00182e720b1db6
Eugene Mirotin <emirotin@gmail.com>
2024-12-01 19:24:36 +0200

    01/01

---
+++ 01/01.ts
@@ -0,0 +1,13 @@
+import { readLines, compNumbers } from "./util.js";
+
+const data = readLines("./input.txt");
+const pairs = data.map((s) => s.split(/\s+/).map((s) => Number.parseInt(s)));
+
+const first = pairs.map(([x]) => x as number).sort(compNumbers);
+const second = pairs.map(([_, y]) => y as number).sort(compNumbers);
+
+console.log(
+  first
+    .map((x, i) => Math.abs(x - (second[i] as number)))
+    .reduce((acc, x) => acc + x, 0)
+);
---
+++ 01/util.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs";
+import path from "node:path";
+
+export const readLines = (fileName: string) => {
+  const data = fs.readFileSync(
+    path.resolve(import.meta.dirname, fileName),
+    "utf-8"
+  );
+  return data
+    .trim()
+    .split("\n")
+    .map((s) => s.trim());
+};
+
+export const compNumbers = (a: number, b: number) => a - b;
