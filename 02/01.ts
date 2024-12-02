cfec0c9d795cdf5fa1634af96d6d9640407d8df1
Eugene Mirotin <emirotin@gmail.com>
2024-12-02 18:45:38 +0200

    02/01

---
+++ 02/01.ts
@@ -0,0 +1,17 @@
+import { readLines, parseNumbers } from "./util.js";
+
+const data = readLines("./input.txt").map(parseNumbers);
+
+const isBetween = (a: number, b: number) => (x: number) => x >= a && x <= b;
+
+const isSafe = (ns: number[]) => {
+	const [a, b] = ns;
+	if (a === b) {
+		return false;
+	}
+
+	const isSafeStep = a < b ? isBetween(1, 3) : isBetween(-3, -1);
+	return ns.every((x, i) => i === 0 || isSafeStep(x - ns[i - 1]));
+};
+
+console.log(data.filter(isSafe).length);
