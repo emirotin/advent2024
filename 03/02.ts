e857c3e33d95b39521e43620bd2df502391be8f1
Eugene Mirotin <emirotin@gmail.com>
2024-12-03 16:37:09 +0200

    03/02

---
+++ 03/02.ts
@@ -0,0 +1,28 @@
+import { readLines } from "./util";
+
+const input = readLines("input.txt").join(" ");
+
+const re = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g;
+
+let res = 0;
+let enabled = true;
+for (const m of input.matchAll(re)) {
+	if (m[0] === "do()") {
+		enabled = true;
+		continue;
+	}
+
+	if (m[0] === "don't()") {
+		enabled = false;
+		continue;
+	}
+
+	if (!enabled) {
+		continue;
+	}
+
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	res += Number.parseInt(m[1]!) * Number.parseInt(m[2]!);
+}
+
+console.log(res);
