77cc77300d7d6467642e3082b008a8de76c77db6
Eugene Mirotin <emirotin@gmail.com>
2024-12-07 11:52:24 +0200

    07/01

---
+++ 07/01.ts
@@ -0,0 +1,31 @@
+import { parseNumbers, readLines } from "../util";
+
+const parseLine = (s: string) => {
+	const [res, nums] = s.split(/:\s/);
+	return {
+		res: Number.parseInt(res!),
+		nums: parseNumbers(nums!),
+	};
+};
+
+const data = readLines("./07/input.txt").map(parseLine);
+
+const canSolve = (res: number, nums: number[]): boolean => {
+	if (nums.length === 1) {
+		return res === nums[0];
+	}
+
+	const last = nums.at(-1)!;
+	const rest = nums.slice(0, -1);
+
+	return (
+		(res % last === 0 && canSolve(res / last, rest)) ||
+		(res >= last && canSolve(res - last, rest))
+	);
+};
+
+console.log(
+	data
+		.map(({ res, nums }) => (canSolve(res, nums) ? res : 0))
+		.reduce<number>((acc, x) => acc + x, 0)
+);
