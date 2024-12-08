6c5da9f08d40aba6221bc177b4adec6829cc01ad
Eugene Mirotin <emirotin@gmail.com>
2024-12-08 12:53:52 +0200

    08/01

---
+++ 08/01.ts
@@ -0,0 +1,53 @@
+import { readLines, uniq } from "../util";
+
+const grid = readLines("./08/input.txt").map((s) => s.split(""));
+const rows = grid.length;
+const cols = grid[0]!.length;
+
+type Point = { r: number; c: number };
+
+const gridWithPos = grid.flatMap((row, r) =>
+	row.map((m, c) => ({ m, pos: { r, c } as Point }))
+);
+
+const uniqueMarkers = uniq(gridWithPos.map(({ m }) => m)).filter(
+	(x) => x !== "."
+);
+
+const getAntinodes = (p1: Point, p2: Point) => {
+	const vec = [p2.r - p1.r, p2.c - p1.c] as const;
+
+	const a1: Point = {
+		r: Math.round(p1.r - vec[0]),
+		c: Math.round(p1.c - vec[1]),
+	};
+	const a2: Point = {
+		r: Math.round(p2.r + vec[0]),
+		c: Math.round(p2.c + vec[1]),
+	};
+
+	return [a1, a2];
+};
+
+const antinodes: Point[] = [];
+
+for (const m of uniqueMarkers) {
+	const positions = gridWithPos
+		.filter((cell) => cell.m === m)
+		.map(({ pos }) => pos);
+
+	const l = positions.length;
+	for (let i = 0; i < l; i++) {
+		for (let j = i + 1; j < l; j++) {
+			antinodes.push(...getAntinodes(positions[i]!, positions[j]!));
+		}
+	}
+}
+
+console.log(
+	uniq(
+		antinodes
+			.filter((a) => a.r >= 0 && a.r < rows && a.c >= 0 && a.c < cols)
+			.map(({ r, c }) => `${r}:${c}`)
+	).length
+);
