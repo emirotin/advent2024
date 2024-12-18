08bb2405d5ce035555218874616dc9206e2a534e
Eugene Mirotin <emirotin@gmail.com>
2024-12-18 11:53:49 +0200

    cleanup

--- 17/02.ts
+++ 17/02.ts
@@ -30,11 +30,11 @@ const solve = (as: number[], bs: number[]): number[] | null => {
 		if (b === 0) {
 			c = a;
 		} else if (b === 1) {
-			c = (a >> 1) + 4 * as[0];
+			c = (a >> 1) + 4 * as[0]!;
 		} else if (b === 2) {
-			c = (a >> 2) + 2 * as[0] + 4 * as[1];
+			c = (a >> 2) + 2 * as[0]! + 4 * as[1]!;
 		} else {
-			c = as[b - 3] + 2 * as[b - 2] + 4 * as[b - 1];
+			c = as[b - 3]! + 2 * as[b - 2]! + 4 * as[b - 1]!;
 		}
 		b = b ^ c ^ 6;
 
@@ -56,26 +56,16 @@ const solve = (as: number[], bs: number[]): number[] | null => {
 	return null;
 };
 
-// we shift A to the right by B bits, B is max 7, so we need extra 10 bits to the left
-// spoiler: it works with 0 seed, but I guess it's a coincidence (nicely-prepared problem data)
-for (let origSeed = 0; origSeed < 2 ** 10; origSeed++) {
-	const seedArray: number[] = [];
-	let seed = origSeed;
-	while (seed > 0) {
-		seedArray.push(seed & 1);
-		seed = seed >> 1;
-	}
-	const as = solve(seedArray, bs);
+const as = solve([0, 0, 0, 0, 0, 0, 0], bs);
 
-	if (as) {
-		let a = 0;
-		for (let i = as.length - 1; i >= 0; i--) {
-			a = a * 2 + as[i];
-		}
-		console.log(a);
-		process.exit(0);
+if (as) {
+	let a = 0;
+	for (let i = as.length - 1; i >= 0; i--) {
+		a = a * 2 + as[i]!;
 	}
+	console.log(a);
+	process.exit(0);
+} else {
+	console.error("Solution not found");
+	process.exit(1);
 }
-
-console.error("Solution not found");
-process.exit(1);
